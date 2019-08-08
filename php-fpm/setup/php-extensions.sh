190f71c3875f9279a4b0b078fa2d1d16683f4a6b
bonimat <m.boni@unibo.it>
2019-08-08 10:23:24 +0200

    Rimosso pacchetti php che non avevano più candidati da installare : libcurl3 libicu57

--- php-fpm/setup/php-extensions.sh
+++ php-fpm/setup/php-extensions.sh
@@ -16,7 +16,7 @@ PACKAGES_POSTGRES="libpq5"
 PACKAGES_MYMARIA="libmariadbclient18"
 
 # Packages for other Moodle runtime dependenices.
-PACKAGES_RUNTIME="ghostscript libaio1 libcurl3 libgss3 libicu57 libmcrypt-dev libxml2 libxslt1.1 \
+PACKAGES_RUNTIME="ghostscript libaio1 libgss3 libmcrypt-dev libxml2 libxslt1.1 \
   libzip-dev locales sassc unixodbc unzip zip"
 
 # Packages for Memcached.
