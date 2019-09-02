41337a5a325b99930d4d6b4d1e8fe9b751492555
bonimat <m.boni@unibo.it>
2019-09-02 10:18:53 +0200

    Add configuration of jenkins service on config.sh file

--- config.sh
+++ config.sh
@@ -6,11 +6,15 @@ set -e
 unset DEVSTACK_DB
 unset DS_PGDATA
 unset DEVSTACK_WWWROOT
+unset DEVSTACK_JENKINS_HOME
+unset DEVSTACK_JENKINS_PORT
 
 # Settaggi di configurazione
 export DEVSTACK_DB=pgsql
 export DS_PGDATA="/home/matteo/Workspaces/Supporto/DatabaseDiSviluppo/pgdata"
 export DEVSTACK_WWWROOT=$(echo $PHPP)
+export DEVSTACK_JENKINS_HOME="/home/matteo/Workspaces/JenkinsProjects"
+#export DEVSTACK_JENKINS_PORT="18080"
 
 # Visualizzazione dei settaggi
 echo $DEVSTACK_DB
