2421a02803a14910f9fafe1a03bec2d1a4720e50
Matteo Boni <bonimat@hotmail.com>
2019-10-20 21:35:09 +0200

    Fix oci8 configuration

--- php-fpm/setup/oci8-extensions.sh
+++ php-fpm/setup/oci8-extensions.sh
@@ -6,16 +6,20 @@ echo "Downloading oracle files"
 
 unzip /tmp/instantclient-basic-linux.x64-*.zip -d /usr/local/
 rm /tmp/instantclient-basic-linux.x64-*.zip
-unzip /tmp/instantclient-sdk-linux.x64-*.zip -d /usr/local/
-rm /tmp/instantclient-sdk-linux.x64-*.zip
+unzip /tmp/instantclient-tools-linux.x64-*.zip -d /usr/local/
+rm /tmp/instantclient-tools-linux.x64-*.zip
 unzip /tmp/instantclient-sqlplus-*.zip -d /usr/local/
 rm /tmp/instantclient-sqlplus-*.zip
-
+unzip /tmp/instantclient-sdk-*.zip -d /usr/local/
+rm /tmp/instantclient-sdk-*.zip
 
 ln -s /usr/local/instantclient_19_3 /usr/local/instantclient
-ln -s /usr/local/instantclient/libclntsh.so.13.1 /usr/local/instantclient/libclntsh.so
+# ln -s /usr/local/instantclient/libclntsh.so.19.1 /usr/local/instantclient/libclntsh.so
 ln -s /usr/local/instantclient/sqlplus /usr/bin/sqlplus
 
+
+cp /tmp/*.ora $ORACLE_HOME/.
+
+
 echo 'instantclient,/usr/local/instantclient' | pecl install oci8 && docker-php-ext-enable oci8
-echo 'oci8.statement_cache_size = 0' >> /usr/local/etc/php/conf.d/docker-php-ext-oci8.ini
-~                                                                                            
+echo 'oci8.statement_cache_size = 0' >> /usr/local/etc/php/conf.d/docker-php-ext-oci8.ini                                                                                            
