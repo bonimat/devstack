237963ab97cfc903912a49432b595311ceef76e3
Matteo Boni <bonimat@hotmail.com>
2019-10-20 19:12:38 +0200

    Add script to test oracle connection

---
+++ html/oracle_connection.php
@@ -0,0 +1,23 @@
+<?php
+
+// Connects to the XE service (i.e. database) on the "localhost" machine
+$conn = oci_connect('moodle.oracle-db[MOODLE]', 'KTiR9xm2', 'app_moodle_con');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM employees');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+?>
