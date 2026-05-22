246f25adc365888c939593176858435773821cd7
Matteo Boni <m.boni@unibo.it>
2026-05-22 11:46:31 +0200

    Fix for 8.3

--- php-fpm/setup/php-extensions.sh
+++ php-fpm/setup/php-extensions.sh
@@ -5,7 +5,7 @@ set -e
 echo "Installing apt dependencies"
 
 # Build packages will be added during the build, but will be removed at the end.
-BUILD_PACKAGES="gettext gnupg libcurl4-openssl-dev libfreetype6-dev libicu-dev libjpeg62-turbo-dev \
+BUILD_PACKAGES="gettext gnupg libcurl4-openssl-dev libfreetype6-dev libjpeg62-turbo-dev \
   libldap2-dev libmemcached-dev libpng-dev libpq-dev libxml2-dev libxslt-dev \
   unixodbc-dev uuid-dev"
 
@@ -17,14 +17,15 @@ PACKAGES_POSTGRES="libpq5"
 #PACKAGES_MYMARIA="libmariadb3"
 
 # Packages for other Moodle runtime dependenices.
-PACKAGES_RUNTIME="ghostscript libaio1 libcurl4 libgss3 libmcrypt-dev libxml2 libxslt1.1 \
-  libzip-dev locales sassc unixodbc unzip zip"
+PACKAGES_RUNTIME="ghostscript libcurl4 libgss3 libmcrypt-dev libxml2 libxslt1.1 \
+  libzip-dev locales sassc unixodbc unzip zip libicu-dev
+"
 
 # Packages for Memcached.
 PACKAGES_MEMCACHED="libmemcached11 libmemcachedutil2"
 
 # Packages for LDAP.
-PACKAGES_LDAP="libldap-2.4-2"
+#PACKAGES_LDAP="libldap-2.4-2"
 
 apt-get update
 apt-get install -y --no-install-recommends apt-transport-https \
@@ -32,8 +33,7 @@ apt-get install -y --no-install-recommends apt-transport-https \
     $PACKAGES_POSTGRES \
     $PACKAGES_MYMARIA \
     $PACKAGES_RUNTIME \
-    $PACKAGES_MEMCACHED \
-    $PACKAGES_LDAP
+    $PACKAGES_MEMCACHED
 
 # Generate the locales configuration fo rboth Australia, and the US.
 echo 'Generating locales..'
@@ -43,38 +43,39 @@ echo 'en_AU.UTF-8 UTF-8' >> /etc/locale.gen
 
 locale-gen
 
-echo "Installing php extensions"
-docker-php-ext-install -j$(nproc) \
-    intl \
-    mysqli \
-    opcache \
-    pgsql \
-    pdo_pgsql \
-    soap \
-    xsl \
-    xmlrpc \
+PHP_EXTENSIONS=(
+    mysqli
+    opcache
+    pgsql
+    pdo_pgsql
+    soap
+    xsl
     bcmath
+    intl
+    zip
+)
 
-# GD.
+for ext in "${PHP_EXTENSIONS[@]}"; do
+  
+    echo "Installing extension: $ext"
+  
+    docker-php-ext-install -j"$(nproc)" "$ext"
+done
 
 docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
 
 docker-php-ext-install -j$(nproc) gd
 
 # LDAP.
-docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
-docker-php-ext-install -j$(nproc) ldap
+#docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/
+#docker-php-ext-install -j$(nproc) ldap
 
 # Note solr is missing. Ref: https://github.com/moodlehq/moodle-php-apache/issues/19
 
-# Memcached, MongoDB, Redis, APCu, igbinary.
-pecl install memcached mongodb redis apcu igbinary solr uuid
-docker-php-ext-enable memcached mongodb redis apcu igbinary solr uuid
+# Memcached, MongoDB, Redis, APCu, igbinary, uuid.
+pecl install memcached mongodb redis apcu igbinary uuid
+docker-php-ext-enable memcached mongodb redis apcu igbinary uuid
 
-# ZIP
-
-docker-php-ext-configure zip --with-zip
-docker-php-ext-install zip
 
 echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
 
